8692d164a6d5a9b606237b029f9d5a54cf44ecd2
HibaAqqaoui <h.aqqaoui@aui.ma>
2023-12-09 20:54:54 +0100

    Added consumer.js

--- src/Consumer/local/consumer.js
+++ src/Consumer/local/consumer.js
@@ -1,94 +1,55 @@
-const axios = require('axios');
+import { OpenApiDefinition } from 'open_api_definition';
 
-class ControllerApiClient {
-    constructor() {
-        this.baseURL = 'http://localhost:3000/api';
-    }
+const apiInstance = new OpenApiDefinition.ControllerApi();
 
-    async browseRemoteDirectory(path) {
-        try {
-            const response = await axios.get(`${this.baseURL}/browse_remote_directory`, { params: { path } });
-            console.log('Browse Response:', response.data);
-            return response.data;
-        } catch (error) {
-            console.error('Error during browse:', error);
-            throw error;
-        }
-    }
 
-    async deleteRemoteFile(path) {
-        try {
-            const response = await axios.delete(`${this.baseURL}/delete_remote_file`, { params: { path } });
-            console.log('Delete Response:', response.data);
-            return response.data;
-        } catch (error) {
-            console.error('Error during delete:', error);
-            throw error;
-        }
-    }
-
-    async downloadRemoteFile(path) {
-        try {
-            const response = await axios.get(`${this.baseURL}/download_remote_file`, { params: { path } });
-            console.log('Download Response:', response.data);
-            return response.data;
-        } catch (error) {
-            console.error('Error during download:', error);
-            throw error;
-        }
-    }
-
-    async renameRemoteFile(oldPath, newPath) {
-        try {
-            const response = await axios.put(`${this.baseURL}/rename_remote_file`, { oldPath, newPath });
-            console.log('Rename Response:', response.data);
-            return response.data;
-        } catch (error) {
-            console.error('Error during rename:', error);
-            throw error;
-        }
-    }
-
-    async uploadLocalFile(remotePath, body) {
-        try {
-            const response = await axios.post(`${this.baseURL}/upload_local_file`, { remotePath, body });
-            console.log('Upload Response:', response.data);
-            return response.data;
-        } catch (error) {
-            console.error('Error during upload:', error);
-            throw error;
-        }
-    }
-}
-
-async function main() {
-    const apiClient = new ControllerApiClient();
-
+async function browseRemoteDirectoryAsync(path) {
     try {
-        const pathToBrowse = '/path/to/browse';
-        const browseResponse = await apiClient.browseRemoteDirectory(pathToBrowse);
-
-        const pathToDelete = '/path/to/delete';
-        const deleteResponse = await apiClient.deleteRemoteFile(pathToDelete);
-
-        const pathToDownload = '/path/to/download';
-        const downloadResponse = await apiClient.downloadRemoteFile(pathToDownload);
-
-        const oldPath = '/path/to/old';
-        const newPath = '/path/to/new';
-        const renameResponse = await apiClient.renameRemoteFile(oldPath, newPath);
-
-        const remotePath = '/path/on/server';
-        const uploadBody = {   file: {
-
-                data: '',
-                name: 'kitten.jpg',
-                type: 'image/jpeg',
-            } };
-        const uploadResponse = await apiClient.uploadLocalFile(remotePath, uploadBody);
+        const response = await apiInstance.browseRemoteDirectory(path);
+        console.log('API called successfully. Returned data:', response.data);
     } catch (error) {
-        console.error('An error occurred:', error);
+        console.error('Error calling API:', error);
     }
 }
 
-main();
+
+async function deleteRemoteFileAsync(path) {
+    try {
+        const response = await apiInstance.deleteRemoteFile(path);
+        console.log('API called successfully. Returned data:', response.data);
+    } catch (error) {
+        console.error('Error calling API:', error);
+    }
+}
+
+
+async function downloadRemoteFileAsync(path) {
+    try {
+        const response = await apiInstance.downloadRemoteFile(path);
+        // Handle the Blob data as needed
+        console.log('API called successfully. Returned data:', response);
+    } catch (error) {
+        console.error('Error calling API:', error);
+    }
+}
+
+
+async function renameRemoteFileAsync(oldPath, newPath) {
+    try {
+        const response = await apiInstance.renameRemoteFile(oldPath, newPath);
+        console.log('API called successfully. Returned data:', response.data);
+    } catch (error) {
+        console.error('Error calling API:', error);
+    }
+}
+
+
+async function uploadLocalFileAsync(remotePath, body) {
+    try {
+        const response = await apiInstance.uploadLocalFile(remotePath, { body });
+        console.log('API called successfully. Returned data:', response.data);
+    } catch (error) {
+        console.error('Error calling API:', error);
+    }
+}
+
